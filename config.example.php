06bddd11e3c35ef5f6226dfc151f720f0e080ac2
X Caminhante <xcaminhante@i2pmail.com>
2025-08-12 21:43:47 -0300

    The right thing

--- config.example.php
+++ config.example.php
@@ -1,7 +1,7 @@
 <?
 $settings = array(
   'title' => 'File Upload Service',
-  'url' => O\s("{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['SERVER_NAME']}{$_SERVER['REQUEST_URI']}")->replace("index.php",""),
+  'url' => O\s("{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}")->replace("index.php",""),
   'lang' => 'en',
   'lang_dir' => 'ltr',
   'random_name_alphabet' => 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-',
