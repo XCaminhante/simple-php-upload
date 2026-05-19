a1a8235c5861234a9fba5de97a6df401375939c8
X Caminhante <xcaminhante@i2pmail.org>
2026-05-19 15:46:01 -0300

    Simple-php-upload now uses O\FileClass Upload redirect time decreased

--- index.php
+++ index.php
@@ -23,289 +23,6 @@ function html () {
 function plain_text () {
   header('Content-type: text/plain; charset=UTF-8');
 }
-//NODE * class File
-class File {
-  private const link = 0120000;
-  private const file = 0100000;
-  private const block = 0060000;
-  private const directory = 0040000;
-  private const fifo = 0010000;
-  private $filename = '';
-  private $pathinfo = '';
-  private $filehandle = false;
-  private $openmode = '';
-  //NODE function __construct ($filename)
-  function __construct ($filename) {
-    $this->filename = O\s($filename);
-    $this->pathinfo = pathinfo($filename);
-  }
-  //NODE static function get_straight_path ($path)
-  static function get_straight_path ($path) {
-    $path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
-    $parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
-    $absolutes = array();
-    foreach ($parts as $part) {
-      if ('.' == $part) continue;
-      if ('..' == $part) {
-        array_pop($absolutes);
-      } else {
-        $absolutes[] = $part; }}
-    return implode(DIRECTORY_SEPARATOR, $absolutes);
-  }
-  //NODE static function current_dir ()
-  // Directory of the current script
-  static function current_dir () {
-    return dirname(__FILE__);
-  }
-  //NODE ** Informations
-  //NODE static function separator ()
-  static function separator () {
-    return DIRECTORY_SEPARATOR;
-  }
-  //NODE public function is_directory ()
-  public function is_directory () {
-    return is_dir($this->filename);
-  }
-  //NODE public function is_file ()
-  public function is_file () {
-    return is_file($this->filename);
-  }
-  //NODE public function parent ()
-  // The directory containing this file/directory
-  public function parent () {
-    return $this->pathinfo['dirname'];
-  }
-  //NODE public function path ()
-  // Canonical path to file/directory
-  public function path () {
-    if ($this->exists()) { return realpath($this->filename); }
-    return realpath($this->parent()) . File::separator() . $this->name();
-  }
-  //NODE public function name ()
-  public function name () {
-    return $this->pathinfo['basename'];
-  }
-  //NODE public function relative_name ()
-  public function relative_name () {
-    return $this->filename;
-  }
-  //NODE public function extension ()
-  public function extension () {
-    return $this->pathinfo['extension'];
-  }
-  //NODE public function length ()
-  // Length of bytes on file or number of entries on a directory
-  public function length () {
-    if ($this->is_directory()) {
-      $dirlist = scandir($this->filename, SCANDIR_SORT_NONE);
-      return O\a($dirlist)->count() - 2; }
-    if ($this->is_file()) {
-      return filesize($this->filename); }
-    return -1;
-  }
-  //NODE public function exists ()
-  public function exists () {
-    return file_exists($this->filename);
-  }
-  //NODE public function is_open ()
-  public function is_open () {
-    return $filehandle != false;
-  }
-  //NODE public function is_uploaded ()
-  public function is_uploaded () {
-    return is_uploaded_file($this->filename);
-  }
-  //NODE public function can_read ()
-  public function can_read () {
-    return is_readable($this->filename);
-  }
-  //NODE public function can_write ()
-  public function can_write () {
-    return is_writable($this->filename);
-  }
-  //NODE public function creation_time ()
-  public function creation_time () {
-    return filectime($this->filename);
-  }
-  //NODE public function last_modified ()
-  public function last_modified () {
-    return filemtime($this->filename);
-  }
-  //NODE public function last_accessed ()
-  public function last_accessed () {
-    return fileatime($this->filename);
-  }
-  //NODE public function position ()
-  // The file pointer position
-  public function position () {
-    if (!$this->filehandle) { return -1; }
-    return ftell($this->filehandle);
-  }
-  //NODE public function mode ()
-  // A string describing the mode used to open the file
-  public function mode () {
-    return $this->openmode;
-  }
-  //NODE ** Manipulations
-  //NODE public function open ($mode)
-  public function open ($mode) {
-    if ($this->filehandle) { return false; }
-    if ($this->is_directory()) {
-      $this->openmode = O\s("d");
-      $this->filehandle = opendir($this->filename);
-    } else {
-      $this->openmode = O\s($mode);
-      $this->filehandle = fopen($this->filename,$mode); }
-    if (!$this->filehandle) { $this->openmode = ''; }
-    return ($this->filehandle != false);
-  }
-  //NODE public function open_rw ()
-  public function open_rw () {
-    return $this->open("c+b");
-  }
-  //NODE public function close ()
-  public function close () {
-    if (!$this->filehandle) { return false; }
-    if ($this->is_directory()) {
-      closedir($this->filehandle);
-      $r = true; }
-    if ($this->is_file()) {
-      $r = fclose($this->filehandle); }
-    $this->filehandle = false;
-    $this->openmode = '';
-    return $r;
-  }
-  //NODE public function remove ()
-  public function remove () {
-    if ($this->filehandle) { $this->close(); }
-    if ($this->is_file()) { return unlink($this->filename); }
-    if ($this->is_directory()) { return rmdir($this->filename); }
-    return false;
-  }
-  //NODE public function copy_to ($target)
-  public function copy_to ($target) {
-    $this->flush();
-    return copy($this->filename,$target);
-  }
-  //NODE public function rename_to ($target)
-  public function rename_to ($target) {
-    if ($this->filehandle) { $this->close(); }
-    $r = rename($this->filename,$target);
-    if ($r) {
-      $this->filename = O\s($target);
-      $this->pathinfo = pathinfo($target); }
-    return $r;
-  }
-  //NODE public function seek ($pos = 0, $whence = SEEK_CUR)
-  public function seek ($pos = 0, $whence = SEEK_CUR) {
-    if (!$this->filehandle) { return false; }
-    if ($this->is_file()) { return fseek($this->filehandle, $pos, $whence); }
-    return false;
-  }
-  //NODE public function rewind ()
-  public function rewind () {
-    if (!$this->filehandle) { return false; }
-    if ($this->is_file()) { return rewind($this->filehandle); }
-    if ($this->is_directory()) { rewinddir($this->filehandle); return true; }
-    return false;
-  }
-  //NODE public function read ($bytes = 1)
-  public function read ($bytes = 1) {
-    if (!$this->filehandle) { return false; }
-    if ($this->is_file()) { return fread($this->filehandle, $bytes); }
-    if ($this->is_directory()) { return readdir($this->filehandle); }
-    return false;
-  }
-  //NODE public function readln ()
-  public function readln () {
-    if (!$this->filehandle) { return false; }
-    if ($this->is_file()) { return fgets($this->filehandle); }
-    if ($this->is_directory()) { return readdir($this->filehandle); }
-    return false;
-  }
-  //NODE public function read_all ()
-  public function read_all () {
-    if (!$this->filehandle) { return false; }
-    $this->rewind();
-    if ($this->is_file()) { return $this->read($this->length()); }
-    if ($this->is_directory()) {
-      $r = '';
-      for ($i = 1; $i <= $this->length(); $i++) { $r .= readdir($this->filehandle) . "\n"; }
-      return $r; }
-    return false;
-  }
-  //NODE public function write ($str)
-  public function write ($str) {
-    if (!$this->filehandle) { return false; }
-    if ($this->is_file()) { return fwrite($this->filehandle, $str); }
-    return false;
-  }
-  //NODE public function write_all ($lines)
-  public function write_all ($lines) {
-    if (!$this->filehandle) { return false; }
-    if ($this->is_file()) { 
-      ftruncate($this->filehandle, 0);
-      rewind($this->filehandle);
-      foreach ($lines as $line) {
-        $r = $this->write($line);
-        if ($r != count($line)) { return false; }}
-      return true; }
-    return false;
-  }
-  //NODE public function flush ()
-  public function flush () {
-    if (!$this->filehandle) { return false; }
-    if ($this->is_file()) { return fflush($this->filehandle); }
-    return false;
-  }
-  //NODE public function list ($pattern = '*')
-  public function list ($pattern = '*') {
-    $dir = $this->parent();
-    if ($this->is_directory() && $this->exists()) {
-      $dir = $this->name(); }
-    return glob($dir . File::separator() . $pattern, GLOB_MARK & GLOB_NOSORT & GLOB_BRACE);
-  }
-  //NODE public function mkdir ($name = false)
-  public function mkdir ($name = false) {
-    if (!$name && !$this->exists()) {
-      return mkdir($this->filename, 0777, true);
-    } else {
-      return false; }
-    $dir = $this->parent();
-    if ($this->is_directory() && $this->exists()) {
-      $dir = $this->name(); }
-    return mkdir($dir . File::separator() . $name, 0777, true);
-  }
-  //NODE public function mkfile ($name)
-  public function mkfile ($name) {
-    $dir = $this->parent();
-    if ($this->is_directory() && $this->exists()) {
-      $dir = $this->name(); }
-    return touch($dir . File::separator() . $name);
-  }
-  //NODE ** Locks
-  //NODE public function lock_exclusive ()
-  public function lock_exclusive () {
-    if ($this->is_directory()) { return false; }
-    if (!$this->filehandle) { $this->open_rw(); }
-    return ($this->filehandle &&
-      flock($this->filehandle, LOCK_EX|LOCK_NB, $eWouldBlock) &&
-      $eWouldBlock == 0);
-  }
-  //NODE public function lock_shared ()
-  public function lock_shared () {
-    if ($this->is_directory()) { return false; }
-    if (!$this->filehandle) { $this->open_rw(); }
-    return ($this->filehandle &&
-      flock($this->filehandle, LOCK_SH|LOCK_NB, $eWouldBlock) &&
-      $eWouldBlock == 0);
-  }
-  //NODE public function unlock ()
-  public function unlock () {
-    if ($this->filehandle) { return $this->close(); }
-    return false;
-  }
-}
 //NODE * class Template
 class Template {
   private $template;
@@ -332,7 +49,7 @@ class FileTemplate {
   private $keys;
   //NODE function __construct ($filename)
   function __construct ($filename) {
-    $this->file = new File($filename);
+    $this->file = O\f($filename);
     $this->keys = [];
   }
   //NODE public function with ($key, $value)
@@ -396,7 +113,7 @@ class UploadedFile {
   static function format_bytes (int $size) {
     if ($size == 0) { return '0 B'; }
     $base = log($size, 1024);
-    $suffixes = array('B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB');  
+    $suffixes = array('B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB');
     return round(pow(1024, $base-floor($base)), 2).' '.$suffixes[floor($base)];
   }
   //NODE public function url ()
@@ -410,7 +127,7 @@ class UploadedFile {
   //NODE public function creation ()
   public function creation () {
     global $settings;
-    return date($settings['date_format'], $this->file->creation_time());
+    return $this->file->creation_time()->format($settings['date_format']);
   }
   //NODE public function name ()
   public function name () {
@@ -507,16 +224,15 @@ function file_upload_error ($err) {
 }
 //NODE function new_random_filename ($extension)
 function new_random_filename ($extension) {
-  a:
   global $settings;
-  $newname = new RandomName($settings['random_name_alphabet'], $settings['random_name_len']);
-  $ext = O\s($extension)
-    ->trim()
+  $ext = O\s($extension)->trim()
     ->substr(0,$settings['random_name_len']-1)
     ->preg_replace('/[^a-zA-Z0-9_-]/u','');
   if ($ext == '') { $ext = 'txt'; }
+  again:
+  $newname = new RandomName($settings['random_name_alphabet'], $settings['random_name_len']);
   $nfilename = 'uploads/' . $newname->name() . '.' . $ext;
-  if (file_exists($nfilename)) { goto a; }
+  if (file_exists($nfilename)) { goto again; }
   return $nfilename;
 }
 //NODE function redirect_after ($seconds,$url)
@@ -524,15 +240,15 @@ function redirect_after ($seconds,$url) {
   $t = new Template('Refresh: {SECS};url={URL}');
   $t->with('{SECS}',$seconds);
   $t->with('{URL}',$url);
-  header($t->read());
+  header($t->read(), true, 303);
 }
 //NODE function upload_file ($file_data)
 function upload_file ($file_data) {
   global $settings;
-  redirect_after(10,$settings['url']);
+  redirect_after(5,$settings['url']);
   if ($file_data['error'] != UPLOAD_ERR_OK) { file_upload_error($file_data['error']); return; }
-  $file = new File($file_data['tmp_name']);
-  $orig = new File($file_data['name']);
+  $file = O\f($file_data['tmp_name']);
+  $orig = O\f($file_data['name']);
   $nfilename = new_random_filename($orig->extension());
   if ($file->rename_to($nfilename)) {
     echo($settings['url'] . $nfilename . "\n");
@@ -543,8 +259,8 @@ function upload_file ($file_data) {
 function delete_file ($filename) {
   global $settings;
   redirect_after(2,$settings['url']);
-  $file = new File('uploads/' . $filename);
-  if ($file->exists() && O\s($file->path())->pos(File::current_dir().'/uploads/') == 0) {
+  $file = O\f('uploads/' . $filename);
+  if ($file->exists() && O\s($file->path())->pos(O\FileClass::current_dir().'/uploads/') == 0) {
     if ($file->remove()) {
       echo('deleted: '.$file->name());
     } else {
@@ -557,11 +273,9 @@ function delete_file ($filename) {
 if (isset($_POST) && isset($_POST['target']) && isset($_POST['action'])) {
   plain_text();
   switch ($_POST['action']) {
-  case 'delete':
-    delete_file($_POST['target']);
-    break;
-  default:
-    echo 'Error'; }
+    case 'delete': delete_file($_POST['target']); break;
+    default: echo 'Error';
+  }
   exit;
 }
 //NODE ** Upload case:
@@ -583,9 +297,11 @@ $template->with('{TITLE}',$settings['title']);
 $template->with('{URL}',$settings['url']);
 $template->with('{MAX_UPLOAD}',ini_get('upload_max_filesize'));
 $template->with('{MAX_FILES}',ini_get('max_file_uploads'));
-$uploads = new File('uploads');
+$uploads = O\f('uploads');
 $uploads_file_list = $uploads->list();
-$uploaded_files = O\a($uploads_file_list) ->map(function ($i) { return new File($i); });
+$uploaded_files = O\a($uploads_file_list)
+  ->map(function ($i) { $f = O\f($i); if (!$f->is_directory()) {return $f;} })
+  ->filter(function ($f) { return !is_null($f); });
 $ufiles_list = new UploadedFilesList($settings['url'], $uploaded_files);
 $template->with('{FILES_LIST}', $ufiles_list->read());
 echo($template->read());
